cfe22a8ab377a87519eb45baeeb87383e0ddca0f
padmaraju_pinnam <pinnampadmaraju@gmail.com>
2021-01-14 18:20:56 +0530

    JAN 14 cars zone update

--- abc_car/urls.py
+++ abc_car/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings as django_settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls', namespace='pages')),
 ]
+
+if django_settings.DEBUG:
+    urlpatterns += static(django_settings.STATIC_URL, document_root=django_settings.STATIC_ROOT)
+    urlpatterns += static(django_settings.MEDIA_URL, document_root=django_settings.MEDIA_ROOT)
--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,7 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('service/', views.service, name='service'),
 ]
